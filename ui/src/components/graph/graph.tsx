594268433ee0cac70fdbedc70b50506a76470b71
DJ Kim <djkim@linkedin.com>
2022-07-18 11:01:35 -0800

    Add prettier and pre-commit hook

--- ui/src/components/graph/graph.tsx
+++ ui/src/components/graph/graph.tsx
@@ -1,4 +1,9 @@
-import React, { MouseEvent as ReactMouseEvent, useCallback, useEffect, useState, } from 'react';
+import React, {
+  MouseEvent as ReactMouseEvent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import ReactFlow, {
   ConnectionLineType,
   Controls,
@@ -10,19 +15,19 @@ import ReactFlow, {
   isNode,
   Node,
   OnLoadParams,
-  ReactFlowProvider
-} from 'react-flow-renderer';
-import { useSearchParams } from 'react-router-dom';
+  ReactFlowProvider,
+} from "react-flow-renderer";
+import { useSearchParams } from "react-router-dom";
 import LineageNode from "./graphNode";
 import { findNodeInElement, getLayoutedElements } from "./utils";
 
 const nodeTypes = {
-  'custom-node': LineageNode,
+  "custom-node": LineageNode,
 };
 type Props = {
   data: Elements;
   nodeId: string;
-}
+};
 const Graph: React.FC<Props> = ({ data, nodeId }) => {
   const [, setURLSearchParams] = useSearchParams();
 
@@ -68,21 +73,27 @@ const Graph: React.FC<Props> = ({ data, nodeId }) => {
   const highlightPath = (node: Node, check: boolean): void => {
     const checkElements = check ? layoutedElements : elements;
 
-    const incomerIds = new Set([...getIncomers(node, checkElements).map((i) => i.id)]);
-    const outgoerIds = new Set([...getOutgoers(node, checkElements).map((o) => o.id)]);
+    const incomerIds = new Set([
+      ...getIncomers(node, checkElements).map((i) => i.id),
+    ]);
+    const outgoerIds = new Set([
+      ...getOutgoers(node, checkElements).map((o) => o.id),
+    ]);
 
     const values: Elements = [];
     for (let index = 0; index < checkElements.length; index++) {
       const element = checkElements[index];
       let highlight = false;
       if (isNode(element)) {
-        highlight = element.id === node.id
-          || incomerIds.has(element.id)
-          || outgoerIds.has(element.id);
+        highlight =
+          element.id === node.id ||
+          incomerIds.has(element.id) ||
+          outgoerIds.has(element.id);
       } else {
         highlight = element.source === node.id || element.target === node.id;
-        const animated = incomerIds.has(element.source)
-          && (incomerIds.has(element.target) || node.id === element.target);
+        const animated =
+          incomerIds.has(element.source) &&
+          (incomerIds.has(element.target) || node.id === element.target);
 
         highlight = highlight || animated;
       }
@@ -134,9 +145,7 @@ const Graph: React.FC<Props> = ({ data, nodeId }) => {
 
   const onNodeDragStop = (_: ReactMouseEvent, node: Node) => {
     const nodePosition = elementMapping[node.data?.id];
-    const values: Elements = [
-      ...elements,
-    ];
+    const values: Elements = [...elements];
     values[nodePosition] = node;
 
     setElements(values);
@@ -144,31 +153,34 @@ const Graph: React.FC<Props> = ({ data, nodeId }) => {
 
   return (
     <div className="lineage-graph">
-        <ReactFlowProvider>
-          <ReactFlow
-            style={ { height: window.innerHeight - 250, width: "100%" } }
-            elements={ elements }
-            snapToGrid
-            snapGrid={ [15, 15] }
-            zoomOnScroll={ false }
-            onLoad={onLoad}
-            onPaneClick={ onPaneClick }
-            onElementClick={ (_: ReactMouseEvent, element: Node | Edge): void => {
-              if (isNode(element)) {
-                resetHighlight();
-                highlightPath(element, false);
-                setURLSearchParams({ nodeId: element.data.id, featureType: element.data.subtitle });
-              }
-            } }
-            onNodeDragStop={ onNodeDragStop }
-            connectionLineType={ ConnectionLineType.SmoothStep }
-            nodeTypes={ nodeTypes }
-          >
-            <Controls />
-          </ReactFlow>
-        </ReactFlowProvider>
+      <ReactFlowProvider>
+        <ReactFlow
+          style={{ height: window.innerHeight - 250, width: "100%" }}
+          elements={elements}
+          snapToGrid
+          snapGrid={[15, 15]}
+          zoomOnScroll={false}
+          onLoad={onLoad}
+          onPaneClick={onPaneClick}
+          onElementClick={(_: ReactMouseEvent, element: Node | Edge): void => {
+            if (isNode(element)) {
+              resetHighlight();
+              highlightPath(element, false);
+              setURLSearchParams({
+                nodeId: element.data.id,
+                featureType: element.data.subtitle,
+              });
+            }
+          }}
+          onNodeDragStop={onNodeDragStop}
+          connectionLineType={ConnectionLineType.SmoothStep}
+          nodeTypes={nodeTypes}
+        >
+          <Controls />
+        </ReactFlow>
+      </ReactFlowProvider>
     </div>
   );
-}
+};
 
 export default Graph;
